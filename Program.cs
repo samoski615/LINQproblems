3199b753e5f2d573747c8d6083ea4341b577f085
samoski615 <samoski615@gmail.com>
2019-08-29 08:50:30 -0500

    only commit, didn't complete assignment, will go back and complete it

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqproblems
+{
+    class Program
+    {
+        
+        //private static List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+       
+
+        
+
+
+    }
+}
---
+++ Words.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqproblems
+{
+    class Words
+    {
+        public static List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+        public void GetWordsWithTh()
+        {
+                   var getQuery =
+                   from th in words
+                   where Char.IsSymbol(th)
+                   select th;
+        }
+
+
+        public static List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+        public void RemoveDuplicates()
+        {
+            var matchWord =
+                from word in names
+                where word.Any<>
+                
+        }
+
+
+
+       
+
+
+
+
+
+
+    }
+}
